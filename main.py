d4d63f9ebd46d96c3776eefb9de87a02ed32d12f
Jan Luttenberger <jan.luttenberger@gmx.at>
2024-05-15 14:47:13 +0200

    updated config

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ numbers = [1, 2, 3, 4]
 values = ["one", "two", "three", "four"]
 
 
-for number, value in zip(numbers, values):
-    print(number, value)
+pairs = [(number, value) for number, value in zip(numbers, values)]
+
+
+print(pairs)
