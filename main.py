00bdd0e9c2155eca3cfc24c981f6ff4af7d4d32c
Jan Luttenberger <jan.luttenberger@gmx.at>
2024-05-22 08:56:48 +0200

    read image

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 import sys
-
+import cv2 as cv
 
 numbers = [1, 2, 3, 4]
 values = ["one", "two", "three", "four"]
 
 
-pairs = [(number, value) for number, value in zip(numbers, values)]
+pairs = {(number, value) for number, value in zip(numbers, values)}
+
+dict_pairs = dict((number, value) for number, value in zip(numbers, values))
 
 
-print(pairs)
+print(dict_pairs)
+
+img = cv.imread("C:/Users/x4v33r/Downloads/image.jpg")
+cv.imshow("Wallpaper", img)
+k = cv.waitKey(0)  # 0
